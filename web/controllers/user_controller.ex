63c4b6768aadf350a5c0a9082e6c2bd969dbce5e
David Salgado <david@digitalronin.com>
2016-05-08 10:58:51 +0100

    add authenticated endpoint to fetch current user
    and an error handler for unauthorized/unauthenticated
    requests

---
+++ web/controllers/user_controller.ex
@@ -0,0 +1,12 @@
+defmodule Peepchat.UserController do
+  use Peepchat.Web, :controller
+
+  alias Peepchat.User
+  plug Guardian.Plug.EnsureAuthenticated, handler: Peepchat.AuthErrorHandler
+
+  def current(conn, _) do
+    user = Guardian.Plug.current_resource(conn)
+    render(conn, Peepchat.UserView, "show.json", user: user)
+  end
+
+end
